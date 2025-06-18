303fca10a1b7a64c13ddd85bf5e80310983074d3
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-06-17 21:05:05 -0300

    Implement JWT authentication: add AuthController, AuthService, and AuthenticateDto; configure JwtModule with secret and expiration; update dependencies

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repository';
+import { compare } from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const { email, password } = authenticateDto;
+    const user = await this.usersRepository.findUnique({ where: { email } });
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+    const isPasswordMatch = await compare(password, user.password);
+    if (!isPasswordMatch)
+      throw new UnauthorizedException('Invalid credentials');
+    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+    return { accessToken };
+  }
+}
