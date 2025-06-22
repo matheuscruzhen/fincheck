552778b5ae925dde0ba939a21a2a569560572156
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-06-22 18:25:59 -0300

    Implement AuthGuard and update module imports: add AuthGuard to AuthModule and AppModule, configure global guards in main.ts; refactor UsersController and UsersService for consistency.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,15 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { env } from 'src/shared/config/env';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: env.jwtSecret,
+      global: true,
       signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
 })
 export class AuthModule {}
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Controller, Get, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  me(@Request() request: any) {
+    return this.usersService.getUserById('request');
+  }
 }
