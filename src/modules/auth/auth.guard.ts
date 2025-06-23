d8017b68e465a708896d73b9e8107a48cc5c6ec6
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2025-06-23 17:50:17 -0300

    Add Categories module with controller, service, and DTOs; integrate dotenv for environment variables

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -9,12 +9,23 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { env } from 'src/shared/config/env';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('IS_PUBLIC', [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
 
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Controller, Get, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  me(@Request() request: any) {
-    return this.usersService.getUserById('request');
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
